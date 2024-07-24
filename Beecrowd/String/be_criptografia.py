d427d96fb47022c549bb5455b7dbceb6957a439f
AlcantaracomT <alcantara.ta28@gmail.com>
2024-07-24 20:50:04 -0300

    exercicio do beecrownd adicionado, parte de string

--- Beecrowd/String/be_criptografia.py
+++ Beecrowd/String/be_criptografia.py
@@ -1,6 +1,54 @@
-a = 'E'
-ascii_code = ord(a)
+import sys
 
-if  97 >= ascii_code <= 122 or 65 >= ascii_code <= 90:
-    a = chr(ascii_code + 3)
-    print(a)
+def asc(a):
+    for i in range(len(a)):
+        a[i] = ord(a[i])
+    return a
+
+def cesar(a,n):
+    if n:
+        for i in range(len(a)):
+            if 97 <= a[i] <= 122 or 65 <= a[i] <= 90:
+                a[i] = chr(a[i] +3)
+            else:
+                a[i] = chr(a[i])
+        a.reverse()
+    else:
+        for i in range(len(a)):
+            if(32 <= a[i] <= 126):
+                a[i] = chr(a[i] -1 )
+
+def main():
+    
+    o = int(input())
+    a = []
+    
+    for i in range(o):
+        a.append(input())
+
+    tama_str = len(a)
+
+    for i in range(tama_str): 
+        n = True
+        l = list(a[i])
+        asc(l)
+        cesar(l,n)
+
+        tam = len(l) 
+        b = l[:(tam//2)]
+
+        l = l[(tam//2):tam]
+
+        n = False
+        asc(l)
+        cesar(l,n)
+
+
+        l = ''.join(l)
+        b = ''.join(b)
+
+        print(b+l)
+        i -= 1
+
+if __name__ == "__main__":
+    main()
